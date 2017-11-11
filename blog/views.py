2d2521cbd5de4e730aa4e2ba26abbd8e74c8ef78
janieyakz <yakzcharlie@gmail.com>
2017-11-11 14:57:28 +0000

    third commit

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,HttpResponse
-
+from blog.models import Post
 # Create your views here.
 def index(request):
-    return render(request,'blog/post_list.html',{})
+    posts=Post.objects.all()
+    context={'posts':posts}
+    return render(request,'blog/post_list.html',context)
     
     
